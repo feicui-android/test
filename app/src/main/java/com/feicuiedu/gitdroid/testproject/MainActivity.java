f79719a1764aef7e56fcd07d2eb75c8d3900b524
yuanchao <yuanchao@feicuiedu.com>
2016-06-28 14:10:07 +0800

    初始提交

---
+++ app/src/main/java/com/feicuiedu/gitdroid/testproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.feicuiedu.gitdroid.testproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/** 初始创建的*/
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
